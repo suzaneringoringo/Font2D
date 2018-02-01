230df64f96c2a21558b8da0422c2ded046bed6c1
pram <rizki.ihza@gmail.com>
2018-02-01 21:12:55 +0700

    asdf

--- test.cpp
+++ test.cpp
@@ -91,14 +91,15 @@ int main() {
 
                 //gradien < -1
                 else {
-                    int dxdy = y2 - y1 + x1 - x2;
-                    int F = y2 - y1 + x1 - x2;
+                    int dxdy = -(y2 - y1 + x1 - x2);
+                    int F = (y2 - y1 + x1 - x2);
                     for (int y = y2; y <= y1; y++) {
                         gambar[y][x] = '*';
                         if (F > 0) {
                             F += dx;
                         } else {
                             x--;
+                        
                             F += dxdy;
                         }
                     } 
